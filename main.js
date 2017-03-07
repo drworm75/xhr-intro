10cee37af8fb72237930127b6831ffdb3951c2f4
drworm75 <dwgames@gmail.com>
2017-03-06 19:58:04 -0600

    wrote XHR request in JS

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("You are running javascript");
+function exectueThisCodeAfterFileLoaded(){
+	console.log("It Worked!!!")
+}
+
+function exectueThisCodeAfterFileFails() {
+	console.log("Boo-urns!");
+
+}
+
+
+
+
+
+var myRequest = new XMLHttpRequest();
+myRequest.addEventListener("load", exectueThisCodeAfterFileLoaded);
+myRequest.addEventListener("error", exectueThisCodeAfterFileFails);
+myRequest.open("GET", "dinosaurs.json");
+myRequest.send();
+console.log("myRequest", myRequest);
